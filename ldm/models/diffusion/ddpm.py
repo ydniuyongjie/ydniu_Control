d034e917de76a0eb0e5a05f3a1525acf3b467031
ydniuyongjie <niuyongjie@163.com>
2025-08-25 08:32:52 +0000

    modify code

--- ldm/models/diffusion/ddpm.py
+++ ldm/models/diffusion/ddpm.py
@@ -884,7 +884,10 @@ class LatentDiffusion(DDPM):
         loss_simple = self.get_loss(model_output, target, mean=False).mean([1, 2, 3])
         loss_dict.update({f'{prefix}/loss_simple': loss_simple.mean()})
 
-        logvar_t = self.logvar[t].to(self.device)
+        # 确保logvar和t在相同设备上
+        if self.logvar.device != self.device:
+            self.logvar = self.logvar.to(self.device)
+        logvar_t = self.logvar[t]
         loss = loss_simple / torch.exp(logvar_t) + logvar_t
         # loss = loss_simple / torch.exp(self.logvar) + self.logvar
         if self.learn_logvar:
